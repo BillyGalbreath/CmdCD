8c4d5ff78bc845fff65edf2341a3eea353bd3657
William Blake Galbreath <blake.galbreath@gmail.com>
2019-06-14 13:18:50 -0500

    Fix startup errors about missing config.yml

--- src/main/java/net/pl3x/bukkit/cmdcd/configuration/Config.java
+++ src/main/java/net/pl3x/bukkit/cmdcd/configuration/Config.java
@@ -22,7 +22,6 @@ public class Config {
      * Reload the configuration file
      */
     public static void reload(Plugin plugin) {
-        plugin.saveDefaultConfig();
         File configFile = new File(plugin.getDataFolder(), "config.yml");
         config = new YamlConfiguration();
         try {
